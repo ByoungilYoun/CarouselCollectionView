4a896fb739558f1425fd2d88dda7cce01712d5aa
YounByoungil <jimmy7525@naver.com>
2022-03-17 19:05:09 +0900

    Setup Project

---
+++ CarouselCollectionView/CarouselCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarouselCollectionView
+//
+//  Created by 윤병일 on 2022/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: - LifeCycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+
+
+}
+
